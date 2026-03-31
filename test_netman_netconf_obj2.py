014e96e1cf36de891a3544d9dc200ad2e24a82a5
Assaf Boneh <asbo8637@colorado.edu>
2026-03-30 22:00:58 -0600

    test

--- test_netman_netconf_obj2.py
+++ test_netman_netconf_obj2.py
@@ -23,7 +23,16 @@ class TestNetmanNetconf(unittest.TestCase):
         ip = row['Mgmt IP'].values[0]
         user = row['Username'].values[0]
         pwd = row['Password'].values[0]
-        with manager.connect(host=ip, port=830, username=user, password=pwd, hostkey_verify=False, allow_agent=False, look_for_keys=False) as m:
+        with manager.connect(
+            host=ip,
+            port=22,
+            username=user,
+            password=pwd,
+            hostkey_verify=False,
+            device_params={'name': 'iosxr'},
+            allow_agent=False,
+            look_for_keys=True
+        ) as m:
             filter_xml = '''<filter><interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"/></filter>'''
             reply = m.get(filter_xml)
             xml_str = str(reply.xml)
@@ -36,28 +45,55 @@ class TestNetmanNetconf(unittest.TestCase):
             net_connect.enable()
             output = net_connect.send_command('show running-config | include area')
             areas = set()
-            for line in output.splitlines():
-                if 'area' in line:
-                    parts = line.split()
-                    if 'area' in parts:
-                        idx = parts.index('area')
-                        if idx+1 < len(parts):
-                            areas.add(parts[idx+1])
-            self.assertEqual(len(areas), 1, f"FAILURE! R1 is configured for multiple areas")
+            import unittest
+            import pandas as pd
+            from ncclient import manager
 
-    def test_ping_r2_to_r5(self):
-        r2 = self.get_device('R2')
-        r5 = self.get_device('R5')
-        ip5 = self.info[self.info['Router'] == 'R5']['Loopback IP'].values[0]
-        with ConnectHandler(**r2) as net_connect:
-            net_connect.enable()
-            output = net_connect.send_command(f'ping {ip5}')
-            success = False
-            for line in output.splitlines():
-                if 'Success rate is' in line:
-                    if '0 percent' not in line:
-                        success = True
-            self.assertTrue(success, f"FAILURE: Ping from R2 to R5's loopback failed. Output: {output}")
+            class TestNetmanNetconf(unittest.TestCase):
+                @classmethod
+                def setUpClass(cls):
+                    cls.info = pd.read_csv('info.csv')
 
-if __name__ == '__main__':
-    unittest.main()
+                def netconf_connect(self, router_name):
+                    row = self.info[self.info['Router'] == router_name]
+                    ip = row['Mgmt IP'].values[0]
+                    user = row['Username'].values[0]
+                    pwd = row['Password'].values[0]
+                    return manager.connect(
+                        host=ip,
+                        port=22,
+                        username=user,
+                        password=pwd,
+                        hostkey_verify=False,
+                        device_params={'name': 'iosxr'},
+                        allow_agent=False,
+                        look_for_keys=True
+                    )
+
+                def test_loopback_r3(self):
+                    with self.netconf_connect('R3') as m:
+                        filter_xml = '''<filter><interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"/></filter>'''
+                        reply = m.get(filter_xml)
+                        xml_str = str(reply.xml)
+                        self.assertIn('Loopback99', xml_str)
+                        self.assertIn('10.1.3.1', xml_str)
+
+                def test_single_area_r1(self):
+                    with self.netconf_connect('R1') as m:
+                        filter_xml = '''<filter><ospf xmlns="urn:ietf:params:xml:ns:yang:ietf-ospf"/></filter>'''
+                        reply = m.get(filter_xml)
+                        xml_str = str(reply.xml)
+                        areas = xml_str.count('area')
+                        self.assertEqual(areas, 1, f"FAILURE! R1 is configured for multiple areas")
+
+                def test_ping_r2_to_r5(self):
+                    with self.netconf_connect('R2') as m:
+                        filter_xml = '''<filter><interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"/></filter>'''
+                        reply = m.get(filter_xml)
+                        xml_str = str(reply.xml)
+                        r5 = self.info[self.info['Router'] == 'R5']
+                        ip5 = r5['Loopback IP'].values[0]
+                        self.assertIn(ip5, xml_str)
+
+            if __name__ == '__main__':
+                unittest.main()
